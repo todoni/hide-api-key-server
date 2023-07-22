32a0512c24f915f40719f7a731138c0547f0240e
sohan <sohan@student.42seoul.kr>
2023-07-22 14:54:37 +0900

    Test

--- server.js
+++ server.js
@@ -47,28 +47,44 @@ app.post("/google-tts", async (req, res) => {
 });
 
 app.post("/google-stt", async (req, res) => {
-	/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-
   const audioContent = req.body;
 
   try {
-    
-	  recorder
-  .record({
-    sampleRateHertz: sampleRateHertz,
-    threshold: 0,
-    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
-    verbose: false,
-    recordProgram: 'rec', // Try also "arecord" or "sox"
-    silence: '10.0',
-  })
-  .stream()
-  .on('error', console.error)
-  .pipe(recognizeStream);
+    const request = {
+      config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+      },
+      interimResults: false, // If you want interim results, set this to true
+    };
 
-console.log('Listening, press Ctrl+C to stop.');
+    // Create a recognize stream
+    const recognizeStream = client
+      .streamingRecognize(request)
+      .on("error", console.error)
+      .on("data", (data) =>
+        process.stdout.write(
+          data.results[0] && data.results[0].alternatives[0]
+            ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+            : "\n\nReached transcription time limit, press Ctrl+C\n"
+        )
+      );
+
+    recorder
+      .record({
+        sampleRateHertz: sampleRateHertz,
+        threshold: 0,
+        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+        verbose: false,
+        recordProgram: "rec", // Try also "arecord" or "sox"
+        silence: "10.0",
+      })
+      .stream()
+      .on("error", console.error)
+      .pipe(recognizeStream);
+
+    console.log("Listening, press Ctrl+C to stop.");
 
     const response = await axios.post(
       `${OTHER_BASE_URL}/endpoint`,
