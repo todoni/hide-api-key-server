f76026876e3273089a6a1b4f9f40240e420668e7
sohan <sohan@student.42seoul.kr>
2023-07-22 13:08:44 +0900

    Feat: OpenAI chat api, Google Cloud TTS api 추가

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const corsConfig = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
 const app = express();
 const PORT = 3000;
 
+dotenv.config();
+
 app.use(bodyParser.json());
+app.use(cors(corsConfig));
 
 app.post("/google-tts", async (req, res) => {
   // 클라이언트로부터 받은 요청 데이터
-  const audioContent = req.body;
+  const audioContent = req.body.audioContent;
+  const endpoint = req.body.endpoint;
+  console.log("req body:", req.body);
 
   try {
     // 내부적으로 Google Cloud Speech-to-Text API를 호출하기 위한 API 키
-    const googleApiKey = "YOUR_GOOGLE_API_KEY";
-    const GOOGLE_BASE_URL = "https://speech.googleapis.com";
+    const API_KEY = process.env.GOOGLE_API_KEY;
+    const BASE_URL = process.env.GOOGLE_TTS_BASE_URL;
 
     const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${googleApiKey}`,
+      "content-type": "applicaton/json; charset=UTF-8",
     };
 
     // Google Cloud Speech-to-Text API 호출
     const response = await axios.post(
-      `${GOOGLE_BASE_URL}/v1/speech:recognize`,
+      `${BASE_URL}${endpoint}?key=${API_KEY}`,
       audioContent,
       { headers }
     );
@@ -67,24 +78,23 @@ app.post("/google-stt", async (req, res) => {
 
 app.post("/openai-chat", async (req, res) => {
   // 클라이언트로부터 받은 요청 데이터
-  const audioContent = req.body;
+  const audioContent = req.body.content;
+  const endpoint = req.body.endpoint;
 
   try {
     // 내부적으로 다른 API를 호출하기 위한 API 키
-    const otherApiKey = "YOUR_OTHER_API_KEY";
-    const OTHER_BASE_URL = "https://api.example.com"; // Replace with the base URL of the other API
+    const API_KEY = process.env.OPENAI_API_KEY;
+    const BASE_URL = process.env.OPENAI_BASE_URL;
 
     const headers = {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${otherApiKey}`,
+      Authorization: `Bearer ${API_KEY}`,
     };
 
     // 다른 API 호출
-    const response = await axios.post(
-      `${OTHER_BASE_URL}/endpoint`,
-      audioContent,
-      { headers }
-    );
+    const response = await axios.post(`${BASE_URL}/${endpoint}`, audioContent, {
+      headers,
+    });
 
     // API 응답을 클라이언트로 전달
     res.status(response.status).json(response.data);
